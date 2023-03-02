68f0d611b96f8e79b5e314f81bce3181001bfcbc
Your NameAQIB-NAWAB <aqibnawab1100@gmail.com>
2023-03-02 11:11:25 -0800

    A simple Login like App

--- src/index.js
+++ src/index.js
@@ -1,17 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { BrowserRouter } from 'react-router-dom';
+import {Routes,Route} from "react-router-dom"
+import EditExercises from './components/edit-exercise.component';
+import ExercisesList from './components/exercises-list.component';
+import CreateExercises from "./components/create-exercise.component"
+import Navbar from "./components/Navbar"
+import CreateUser from "./components/craete-user.component"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <BrowserRouter>
+<div className="container">
+
+
+
+
     <App />
+
+
+    <Routes>
+
+<Route path="/" exact  element={<ExercisesList/>}/>
+<Route path="/edit:id" exact  element={<EditExercises/>}/>
+<Route path="/create" exact  element={<CreateExercises/>}/>
+<Route path="/user" exact  element={<CreateUser/>}/>
+
+
+
+
+    </Routes>
+</div>
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
